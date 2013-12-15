6860dd11fb85ae4af22f9a980283f60f89b71eaa
fiatjaf <fiatjaf@gmail.com>
2013-12-15 20:05:10 -0200

    little turnaround for running this test-like script in node

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,5 @@
+  Tempreites = typeof Tempreites != 'undefined' ? Tempreites : require('../dist/tempreites')
+
   var template = '<!doctype html><div id="habitantes">\n  <input/>\n  <div class="atuais">\n    <h1>Habitantes</h1>\n    <table>\n      <thead>\n        <tr>\n          <th></th>\n          <th></th>\n          <th></th>\n        </tr>\n      </thead>\n      <tbody class="ativos">\n        <tr>\n          <td class="quarto"></td>\n          <td>\n            <a class="nome" data-bind-here="href" data-bind-there="link"></a>\n          </td>\n        </tr>\n      </tbody>\n    </table>\n  </div>\n  <div class="antigos">\n    <h1>habitantes antigos</h1>\n    <table>\n      <thead>\n        <tr>\n          <th></th> \n          <th></th> \n        </tr> \n      </thead>\n      <tbody class="inativos">\n        <tr>\n          <td class="quarto"></td>\n          <td>\n            <a class="nome" data-bind-here="href" data-bind-there="link"></a>\n          </td>\n        </tr>\n      </tbody> \n    </table>\n  </div>\n</div>\n' 
   var data = { 
     ativos: [{ 
